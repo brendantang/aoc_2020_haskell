6ca949108ee1ebf978fa708bd8e19310bc2de66d
Brendan Tang <b@brendantang.net>
2021-02-27 13:56:14 -0500

    Remove unnecessary lines from d6p2

--- day6/pt2.hs
+++ day6/pt2.hs
@@ -1,5 +1,5 @@
 module Main where
-import Data.List (nub, delete, intersect)
+import Data.List (intersect)
 
 -- Copy-pasted from day 4
 splitOnEmptyLine :: String -> [String]
@@ -10,14 +10,9 @@ splitOnEmptyLine s = firstElem : restOfTheElems
             | null remainingLines = []
             | otherwise = splitOnEmptyLine $ unlines $ tail remainingLines
 
-countUniqueLetters :: String -> Int
-countUniqueLetters s = length (delete '\n' $ nub s)
-
 main = do
     inputText <- readFile "data.txt"
     let groupAnswers = map lines $ splitOnEmptyLine inputText
     let unanimousGroupAnswers = map (foldl intersect ['a'..'z']) groupAnswers
     print unanimousGroupAnswers 
     print $ "Sum of unanimous group answers:" ++ show (sum $ map length unanimousGroupAnswers)
-
-
